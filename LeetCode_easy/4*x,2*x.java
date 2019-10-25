240a66db2e639e1220afaf9a0a4df9a2761607ef
吴楠 <50066750+WuNanya@users.noreply.github.com>
2019-10-25 23:11:28 +0800

    Create 4*x,2*x.java

---
+++ LeetCode_easy/4*x,2*x.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if((num & 0xaaaaaaaa)==0 &&Integer.bitCount(num)==1) return true;
+        else return false;
+    }
+}
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+}
